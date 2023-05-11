ccd0c5a51a0858a41504e33369b9daafb6b1677d
Ariel Higuera <purpl3m00ns@gmail.com>
2023-05-11 14:24:41 -0700

    commented out a few tests

--- tests/merge_sorted_lists_test.py
+++ tests/merge_sorted_lists_test.py
@@ -13,25 +13,25 @@ def test_for_1_and_2():
     assert answer == [1, 2]
 
 
-def test_for_2_and_1():
-    # Arrange
-    l1 = [2]
-    l2 = [1]
+# def test_for_2_and_1():
+#     # Arrange
+#     l1 = [2]
+#     l2 = [1]
 
-    # Act
-    answer = merge_sorted_lists(l1, l2)
+#     # Act
+#     answer = merge_sorted_lists(l1, l2)
 
-    # Assert
-    assert answer == [1, 2]
+#     # Assert
+#     assert answer == [1, 2]
 
 
-def test_for_1_2_3_and_4_5_6():
-    # Arrange
-    l1 = [1, 2, 3]
-    l2 = [4, 5, 6]
+# def test_for_1_2_3_and_4_5_6():
+#     # Arrange
+#     l1 = [1, 2, 3]
+#     l2 = [4, 5, 6]
 
-    # Act
-    answer = merge_sorted_lists(l1, l2)
+#     # Act
+#     answer = merge_sorted_lists(l1, l2)
 
-    # Assert
-    assert answer == [1, 2, 3, 4, 5, 6]
+#     # Assert
+#     assert answer == [1, 2, 3, 4, 5, 6]
